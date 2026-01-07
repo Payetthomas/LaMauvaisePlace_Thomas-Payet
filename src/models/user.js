cc400d7b3aad89fcc3cdb50a4d22a31fb292298a
Thomas Payet <thomaspayet.tp@gmail.com>
2026-01-07 16:38:57 +0100

    ajout des colonnes necessaires pour loginet d'une valeur par defaut des roles

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+
+const User = (sequelize, DataTypes) => {
+
+    class User extends Model {
+
+    }
+
+    User.init(
+        {
+            username: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING,
+            firstname: DataTypes.STRING,
+            lastname: DataTypes.STRING,
+            profile_picture_url: DataTypes.STRING,
+            phone_number: DataTypes.STRING, 
+            address: DataTypes.STRING,
+            zip_code: DataTypes.STRING, 
+            city: DataTypes.STRING, 
+            token: DataTypes.TEXT,
+            role: {
+                type: DataTypes.ENUM,
+                values: ['admin', 'seller'],
+                defaultValue: 'seller'
+            },
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false
+            },
+            password: DataTypes.STRING
+        },
+        {
+            sequelize,
+            modelName: "User"
+        }
+    )
+
+    return User; 
+}; 
+
+module.exports = User;
